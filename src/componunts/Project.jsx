97add78ef08179acf2e99c99452e3470f438da46
choi ej <cyslisa0211@naver.com>
2025-09-23 15:40:12 +0900

    250923-1

--- src/componunts/Project.jsx
+++ src/componunts/Project.jsx
@@ -43,19 +43,6 @@ function Project() {
             </ul>
         </section>
     );
-    // return(
-    //     <section className="project">
-    //         <h2>Portfolio</h2>
-    //         <ul className="pf-list">
-    //             <li><a href="/">통합공간기획</a></li>
-    //             <li><a href="/">주택조경</a></li>
-    //             <li><a href="/">상업조경</a></li>
-    //             <li><a href="/">공공프로젝트</a></li>
-    //             <li><a href="/">전시/팝업/콘텐츠</a></li>
-    //             <li><a href="/">공유정원</a></li>
-    //         </ul>
-    //     </section>
-    // );
 }
 
 export default Project;
